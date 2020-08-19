65a560f60ac1504d93aeac0c8d94d54b9f68293e
Ray Milkey <ray@opennetworking.org>
2020-08-19 15:37:54 -0700

    Hook up new Aether umbrella chart test to the suite and make it use common address routines (#61)

--- aether-umbrella/tests/aether-umbrella.go
+++ aether-umbrella/tests/aether-umbrella.go
@@ -7,6 +7,7 @@ package tests
 import (
 	"github.com/onosproject/helmit/pkg/helm"
 	"github.com/onosproject/helmit/pkg/test"
+	"github.com/onosproject/onos-test/pkg/onostest"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -16,21 +17,24 @@ type AetherUmbrellaSuite struct {
 	test.Suite
 }
 
+const componentName = "aether-umbrella"
+const testName = "aether-chart-test"
+
 // TestInstall tests installing the aether-umbrella chart
 func (s *AetherUmbrellaSuite) TestInstall(t *testing.T) {
-	atomix := helm.Chart("atomix-controller", "https://charts.atomix.io").
-		Release("aether-umbrella-atomix").
+	atomix := helm.Chart("atomix-controller", onostest.AtomixChartRepo).
+		Release(onostest.AtomixName(testName, componentName)).
 		Set("scope", "Namespace")
 	assert.NoError(t, atomix.Install(true))
 
-	raft := helm.Chart("raft-storage-controller", "https://charts.atomix.io").
-		Release("aether-umbrella-raft").
+	raft := helm.Chart("raft-storage-controller", onostest.AtomixChartRepo).
+		Release(onostest.RaftReleaseName(componentName)).
 		Set("scope", "Namespace")
 	assert.NoError(t, raft.Install(true))
 
 	onos := helm.Chart("aether-umbrella").
 		Release("aether-umbrella").
-		Set("global.storage.controller", "aether-umbrella-atomix-atomix-controller:5679").
+		Set("global.storage.controller", onostest.AtomixController(testName, componentName)).
 		Set("import.onos-gui.enabled", false).
 		Set("import.onos-cli.enabled", false).
 		Set("onos-topo.image.tag", "latest").
